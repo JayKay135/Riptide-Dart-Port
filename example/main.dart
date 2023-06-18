df7a6527b5e93d30426c0a39d1e6b6f5a6ae5ac1
MainPC <st161384@stud.uni-stuttgart.de>
2023-06-18 17:33:41 +0200

    added example

---
+++ example/main.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:riptide/riptide.dart';
+
+enum ClientToServerId { serverTest }
+
+enum ServerToClientId { clientTest }
+
+void createClient() {
+  Client client = Client();
+  client.connect(InternetAddress('127.0.0.1'), 7777);
+
+  client.registerMessageHandler(ServerToClientId.clientTest.index, (Message message) {
+    print(message);
+  });
+
+  client.clientConnected.subscribe((args) {
+    // send a test message to the server on connection
+    Message message = Message.createFromInt(MessageSendMode.reliable, ClientToServerId.serverTest.index);
+    message.addString('Hello Server!');
+    client.send(message);
+  });
+}
+
+void createServer() {
+  Server server = Server();
+  server.start(7777, 10);
+
+  server.registerMessageHandler(ClientToServerId.serverTest.index, (fromClientID, message) {
+    print("received message from client $fromClientID");
+  });
+
+  server.clientConnected.subscribe((args) {
+    // send a test message to each connecting client
+    Message message = Message.createFromInt(MessageSendMode.reliable, ServerToClientId.clientTest.index);
+    message.addString('Hello Client!');
+    server.send(message, args!.client.id);
+  });
+}
